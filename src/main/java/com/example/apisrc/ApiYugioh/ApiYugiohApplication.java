d84868fd4a83ce518304d4bd0e6f997bbc0be77a
Eder venancio <ederpvenancio@gmail.com>
2023-08-01 08:39:42 -0300

    first commit

---
+++ src/main/java/com/example/apisrc/ApiYugioh/ApiYugiohApplication.java
@@ -0,0 +1,13 @@
+package com.example.apisrc.ApiYugioh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiYugiohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiYugiohApplication.class, args);
+	}
+
+}
