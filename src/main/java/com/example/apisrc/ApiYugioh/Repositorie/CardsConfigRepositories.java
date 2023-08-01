9bb934717409a81ece4089f919ee5d28d0b9f341
Eder venancio <ederpvenancio@gmail.com>
2023-08-01 09:50:06 -0300

    add card repositorie

--- src/main/java/com/example/apisrc/ApiYugioh/Repositorie/CardsConfigRepositories.java
+++ src/main/java/com/example/apisrc/ApiYugioh/Repositorie/CardsConfigRepositories.java
@@ -1,12 +1,16 @@
 package com.example.apisrc.ApiYugioh.Repositorie;
 
+import com.example.apisrc.ApiYugioh.Entity.CardsConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CardsConfig extends JpaRepository<CardsConfig, Integer> {
-    List<CardsConfig> findByType(String name);
-    List<CardsConfig> findByAttribute(String name);
+public interface CardsConfigRepositories extends JpaRepository<CardsConfig, Integer> {
+
+    @Query(value = "SELECT cc FROM CardsConfig cc where cc.cardType = ?1")
+    List<CardsConfigRepositories> findByType(String name);
+    List<CardsConfigRepositories> findByAttribute(String name);
 }
