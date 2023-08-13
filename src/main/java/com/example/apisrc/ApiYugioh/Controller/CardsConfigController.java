34949b4b009b52131addc1908c07ef4843a1746f
Eder venancio <ederpvenancio@gmail.com>
2023-08-12 21:49:20 -0300

    add exceptions method

--- src/main/java/com/example/apisrc/ApiYugioh/Controller/CardsConfigController.java
+++ src/main/java/com/example/apisrc/ApiYugioh/Controller/CardsConfigController.java
@@ -2,7 +2,8 @@ package com.example.apisrc.ApiYugioh.Controller;
 
 
 import com.example.apisrc.ApiYugioh.Entity.CardsConfig;
-import com.example.apisrc.ApiYugioh.Repositorie.CardsConfigRepositories;
+import com.example.apisrc.ApiYugioh.Exceptions.CardsConfigErrorResponse;
+import com.example.apisrc.ApiYugioh.Exceptions.CardsConfigNotFoundException;
 import com.example.apisrc.ApiYugioh.Service.CardsConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
@@ -26,13 +28,50 @@ public class CardsConfigController {
 
     @GetMapping(value = "/cardType/{cardType}")
     public ResponseEntity<List<CardsConfig>> findCardByType(@PathVariable String cardType){
-       return new ResponseEntity<>(cardsConfigService.findCardByType(cardType), HttpStatus.OK);
+
+        String found = cardsConfigService.findCardByType(cardType).toString();
+
+        System.out.println(found);
+            if((cardType == "")){
+                throw new CardsConfigNotFoundException("Not found Card Type");
+            }
+                return new ResponseEntity<>(cardsConfigService.findCardByType(cardType), HttpStatus.OK);
+
     }
 
    @GetMapping(value = "/cardAttribute/{cardAttribute}")
    public ResponseEntity<List<CardsConfig>> findCardByAttribute(@PathVariable String cardAttribute){
+
         return new ResponseEntity<>(cardsConfigService.findCardByAttribute(cardAttribute), HttpStatus.OK);
    }
 
+
+   @PostMapping("/cards/add")
+   public CardsConfig addCardsConfig(@RequestBody CardsConfig cardsConfig){
+
+
+        cardsConfig.setIdCardConfig(0);
+        CardsConfig addCardsConfig = cardsConfigService.save(cardsConfig);
+
+        return addCardsConfig;
+   }
+
+   @GetMapping("/cards")
+    public List <CardsConfig> getAllCards(){
+        return cardsConfigService.findAll();
+   }
+
+
+   @ExceptionHandler
+    public ResponseEntity<CardsConfigErrorResponse> handleException (CardsConfigNotFoundException exc){
+        CardsConfigErrorResponse error = new CardsConfigErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+   }
 
 }
