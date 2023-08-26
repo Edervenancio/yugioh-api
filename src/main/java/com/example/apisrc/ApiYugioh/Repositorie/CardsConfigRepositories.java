0fc0bed9c6bab7b1350534a9d8d8d9dfc716a712
Eder venancio <ederpvenancio@gmail.com>
2023-08-26 19:02:19 -0300

    add find by id method

--- src/main/java/com/example/apisrc/ApiYugioh/Repositorie/CardsConfigRepositories.java
+++ src/main/java/com/example/apisrc/ApiYugioh/Repositorie/CardsConfigRepositories.java
@@ -13,12 +13,13 @@ public interface CardsConfigRepositories extends JpaRepository<CardsConfig, Inte
 
     CardsConfig save(CardsConfig cardsConfig);
 
-
-
     @Query(value = "SELECT cc from CardsConfig cc where cc.cardType =?1 and cc.cardAttribute =?2 ")
     CardsConfig findCardByTypeAndAttribute(String cardType, String cardAttribute);
     @Query(value = "SELECT cc FROM CardsConfig cc where cc.cardType = ?1")
     List<CardsConfig> findByType(String cardType);
+
+    @Query(value = "SELECT cc from CardsConfig cc where cc.idCardConfig = ?1")
+    CardsConfig findCardById(int id);
 
     @Query(value = "SELECT cc FROM CardsConfig cc where cc.cardAttribute = ?1")
      List<CardsConfig> findByCardAttribute(String cardAttribute);
@@ -32,8 +33,6 @@ public interface CardsConfigRepositories extends JpaRepository<CardsConfig, Inte
 
     @Query(value = "delete from CardsConfig cc where cc.idCardConfig = ?1")
     void deleteCardConfig(int id);
-
-
 
 
 }
